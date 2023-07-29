8963da8793a90fc6f1f066150f313aaa9f30ded4
Zack Spring Chief <jzacksc@gmail.com>
2023-07-29 11:46:56 -0600

    added a lot more to portfolio

--- script.js
+++ script.js
@@ -26,6 +26,12 @@ window.onscroll = () => {
           .querySelector("header nav a[href*=" + id + "]")
           .classList.add("active");
       });
+      // active section for animation on scroll
+      sec.classList.add("show-animate");
+    }
+    // if you want to use animate on scroll use this
+    else {
+      sec.classList.remove("show-animate");
     }
   });
 
